a3525a9a641471babae1e87233c4acbeb25d17d8
Ricardo <ricardo.gon.tell@gmail.com>
2013-12-17 17:32:15 -0600

    Ruta home

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ NewsT::Application.routes.draw do
       resources :tags
     end
   end
+
+  root 'home#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
